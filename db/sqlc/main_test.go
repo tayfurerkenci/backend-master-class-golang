a1c030e6834652ad6dfe05066e7b559b15b8f820
Tayfur <terkenci@gmail.com>
2023-12-16 21:24:49 +0300

    feat: added golang viper to use env variables for different environments

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,20 +7,19 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
+	"github.com/tayfurerkenci/backend-master-class-golang/util"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:tayfur@localhost:5432/simple_bank?sslmode=disable"
-)
-
 func TestMain(m *testing.M) {
-	var err error
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
 
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
