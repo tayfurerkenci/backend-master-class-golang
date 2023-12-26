ce45b2583a3f867087dc6d56a35da65d548585f8
Tayfur <terkenci@gmail.com>
2023-12-26 22:25:24 +0300

    feat: implemented user api unit tests with custom gomock matcher

--- api/main_test.go
+++ api/main_test.go
@@ -5,7 +5,19 @@ import (
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	db "github.com/tayfurerkenci/backend-master-class-golang/db/sqlc"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	// config := util.Config{
+	// 	TokenSymmetricKey:   util.RandomString(32),
+	// 	AccessTokenDuration: time.Minute,
+	// }
+
+	server := NewServer(store)
+
+	return server
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
