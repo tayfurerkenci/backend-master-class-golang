ebeb322efccd739651d892f311cbb424069a9093
Tayfur <terkenci@gmail.com>
2023-12-28 23:33:32 +0300

    feat: implemented loginUser api

--- api/main_test.go
+++ api/main_test.go
@@ -3,18 +3,22 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
 	db "github.com/tayfurerkenci/backend-master-class-golang/db/sqlc"
+	"github.com/tayfurerkenci/backend-master-class-golang/util"
 )
 
 func newTestServer(t *testing.T, store db.Store) *Server {
-	// config := util.Config{
-	// 	TokenSymmetricKey:   util.RandomString(32),
-	// 	AccessTokenDuration: time.Minute,
-	// }
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
 
-	server := NewServer(store)
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
 
 	return server
 }
